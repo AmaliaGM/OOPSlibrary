630e44a33fe4fac65a7a60997763d1de99fdee8f
amalia gomez moro <mariaamaliagomezmoro@gmail.com>
2022-11-07 19:38:30 -0300

    fix student disable linter

--- student.rb
+++ student.rb
@@ -1,7 +1,7 @@
 require_relative './person'
 
 class Student < Person
-  def initialize(age, name = 'Unknown', parent_permission)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
   end
 
