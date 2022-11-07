61a9a0e675c15450b17a2f063ec23343cc96fb4b
amalia gomez moro <mariaamaliagomezmoro@gmail.com>
2022-11-07 20:35:18 -0300

    update person

--- person.rb
+++ person.rb
@@ -1,10 +1,11 @@
+# rubocop: disable Style/OptionalBooleanParameter
 require_relative './corrector'
 
 class Person
   attr_accessor :name, :age, :rentals
   attr_reader :id
 
-  def initialize(age, name = 'Unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission = true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
@@ -31,3 +32,4 @@ class Person
     of_age? || parent_permission == true
   end
 end
+# rubocop: enable Style/OptionalBooleanParameter
