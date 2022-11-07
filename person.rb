7103fcf2212f327e8b572515046d4bc657c3d46a
amalia gomez moro <mariaamaliagomezmoro@gmail.com>
2022-11-07 19:58:08 -0300

    fix person linters desable

--- person.rb
+++ person.rb
@@ -1,11 +1,10 @@
-# rubocop: disable Style/OptionalBooleanParameter
 require_relative './corrector'
 
 class Person
   attr_accessor :name, :age, :rentals
   attr_reader :id
 
-  def initialize(age, name = 'Unknown', parent_permission = true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
@@ -32,4 +31,3 @@ class Person
     of_age? || parent_permission == true
   end
 end
-# rubocop: enable Style/OptionalBooleanParameter
